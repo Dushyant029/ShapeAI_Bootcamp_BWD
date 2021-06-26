4665f8ebeeaf5100754786f23967d9990f45bd0e
K.DUSHYANT REDDY <dushireddy291@gmail.com>
2021-06-26 17:07:00 +0530

    Proj

---
+++ src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Today's Work</h1>
+      <p>Learn a language,Write & edit code and Work on real-world problems.</p>
+    </div>
+  );
+}
+
+export default Info;
